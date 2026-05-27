a46bc0414b02514ea9ef7456a0cabee94b1a1168
marekwino23 <marekw1996@gmail.com>
2026-05-28 00:14:13 +0200

    new changes

--- src/types/booksSchema.ts
+++ src/types/booksSchema.ts
@@ -1,11 +1,16 @@
 import { z } from "zod";
 
 export const bookSchema = z.object({
-  title: z.string(),
-  author: z.string(),
-  isbn: z.string(),
-  number_of_pages: z.coerce.number().positive(),
-  rating: z.coerce.number().min(1).max(5),
+  title: z.string().min(1, "Tytuł jest wymagany"),
+  author: z.string().min(1, "Autor jest wymagany"),
+  isbn: z.string().min(10, "ISBN musi mieć min. 10 znaków"),
+  number_of_pages: z.coerce
+    .number()
+    .positive("Liczba stron musi być większa od 0"),
+  rating: z.coerce
+    .number()
+    .min(1, "Minimalna ocena to 1")
+    .max(5, "Maksymalna ocena to 5"),
 });
 
 export type BookFormData = z.infer<typeof bookSchema>;
